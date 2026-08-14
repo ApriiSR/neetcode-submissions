fc0b1d9d376a28265cab76b5ee9397e84dc82e95
April <aprilkopec47@gmail.com>
2026-08-13 20:48:54 -0700

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,14 @@
+import math
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        result = 1
+        cars = sorted([(position[i], speed[i]) for i in range(len(position))])
+        lead = cars.pop()
+        while cars:
+            if (target-lead[0])/lead[1] >= (target-cars[-1][0])/cars[-1][1]:
+                cars.pop()
+            else:
+                result += 1
+                lead = cars.pop()
+        return result
