5d7ecbc4115940f8655426dbcc362ab5c3f8ed3e
April <april@apriiori.com>
2026-08-15 12:39:02 -0700

    Implement maxArea function for water container problem

--- Data Structures & Algorithms/max-water-container/submission-0.py
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -1,18 +1,7 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        nums = sorted(nums)
-        for low in range(len(nums)-2):
-            mid = low + 1
-            high = len(nums) - 1
-            while mid < high:
-                if nums[mid] + nums[high] == -nums[low]:
-                    if (nums[low], nums[mid], nums[high]) not in result:
-                        result.append((nums[low], nums[mid], nums[high]))
-                    mid += 1
-                    high -= 1
-                elif nums[mid] + nums[high] < -nums[low]:
-                    mid += 1
-                else:
-                    high -= 1
-        return result
+    def maxArea(self, heights: List[int]) -> int:
+        best = 0
+        for start in range(len(heights)-1):
+            for end in range(start+1, len(heights)):
+                best = max(best, (end-start)*min(heights[start],heights[end]))
+        return best
