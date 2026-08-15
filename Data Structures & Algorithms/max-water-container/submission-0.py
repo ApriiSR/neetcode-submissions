416a7ded461b0a83664841f1d85c3e4ef3a8a9f7
April <april@apriiori.com>
2026-08-15 12:56:06 -0700

    Rename variable 'best' to 'most' in maxArea

--- Data Structures & Algorithms/max-water-container/submission-0.py
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        best = 0
+        most = 0
         for start in range(len(heights)-1):
             for end in range(start+1, len(heights)):
-                best = max(best, (end-start)*min(heights[start],heights[end]))
-        return best
+                most = max(most, (end-start)*min(heights[start],heights[end]))
+        return most
