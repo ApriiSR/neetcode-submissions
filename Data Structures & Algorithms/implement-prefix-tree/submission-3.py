4df52f1269dcf69c5c609586b0ef3b44f0059695
April <aprilkopec47@gmail.com>
2026-08-16 17:24:35 -0700

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.py
@@ -0,0 +1,34 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.children = {}
+        self.present = False
+        sys.setrecursionlimit(2000)
+
+    def insert(self, word: str) -> None:
+        if word and not word[0] in self.children:
+            self.children[word[0]] = PrefixTree()  
+        if word:
+            self.children[word[0]].insert(word[1:])
+        else:
+            self.present = True
+
+    def search(self, word: str) -> bool:
+        if word:
+            if word[0] in self.children:
+                return self.children[word[0]].search(word[1:])
+            else:
+                return False
+        else:
+            return self.present
+
+
+    def startsWith(self, prefix: str) -> bool:
+        if prefix:
+            if prefix[0] in self.children:
+                return self.children[prefix[0]].startsWith(prefix[1:])
+            else:
+                return False
+        else:
+            return True
+        
