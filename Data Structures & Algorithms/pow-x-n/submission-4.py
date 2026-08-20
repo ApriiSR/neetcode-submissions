d3e4549c2bcd5b0ba1740433edcc2ed27abb285a
April <aprilkopec47@gmail.com>
2026-08-20 15:52:15 -0700

    Add: pow-x-n - submission-4

---
+++ Data Structures & Algorithms/pow-x-n/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n == 1:
+            return x
+        elif n > 1:
+            a = x
+            for i in range(n.bit_length()-1):
+                a *= a
+            print(a)
+            a *= self.myPow(x, n - (1 << (n.bit_length()-1)))
+            return a
+        else:
+            return self.myPow(1/x, -n)
