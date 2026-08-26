2ee408bd676be97841783aa193ed0b0705a62db2
April <aprilkopec47@gmail.com>
2026-08-25 21:25:10 -0700

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.py
@@ -0,0 +1,16 @@
+def merge(intervals: List[List[int]]) -> List[List[int]]:
+    intervals.sort(key = lambda x: x[0])
+    i = 0
+    while i + 1 < len(intervals):
+        if intervals[i][1] >= intervals[i+1][0]:
+            intervals[i][1] = max(intervals[i][1], intervals[i+1][1])
+            intervals.pop(i+1)
+        else:
+            i += 1
+    return intervals
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        return merge(intervals + [newInterval])
+        
+        
