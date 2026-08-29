8ec21daeae7a54a5b2f9fec63255ef2c8c1edb6d
April <aprilkopec47@gmail.com>
2026-08-28 20:26:33 -0700

    Add: count-connected-components - submission-6

---
+++ Data Structures & Algorithms/count-connected-components/submission-6.py
@@ -0,0 +1,16 @@
+import sys
+sys.setrecursionlimit(99999999)
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        components = n
+        component = {i: (lambda i=i: i) for i in list(range(n))}
+        for e in edges:
+            a, b = component[e[0]](), component[e[1]]()
+            if component[a]() != component[b]():
+                components -= 1
+                if component[a]() < component[b]():
+                    component[b] = lambda a=a: component[a]()
+                else:
+                    component[a] = lambda b=b: component[b]()
+        return components
